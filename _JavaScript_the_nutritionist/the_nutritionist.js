3e24e2682cd0f1752ab5fd2447f2ec4d389a94a8
codekelvin101 <89131290+codekelvin101@users.noreply.github.com>
2021-09-01 13:35:10 +0300

    Add files via upload

---
+++ _JavaScript_the_nutritionist/the_nutritionist.js
@@ -0,0 +1,311 @@
+/*
+ *	This content is generated from the PSD File Info.
+ *	(Alt+Shift+Ctrl+I).
+ *
+ *	@desc 		
+ *	@file 		the_nutritionist
+ *	@date 		1630491826713
+ *	@title 		Page 1
+ *	@author 	
+ *	@keywords 	
+ *	@generator 	Export Kit v1.3.figma
+ *
+*/
+
+var content_container = document.createElement("div");
+content_container.style.width = "1280px";
+content_container.id = "content_container";
+content_container.style.position = "relative";
+content_container.style.display = "block";
+content_container.style.margin = "0 auto";
+document.body.appendChild(content_container);
+
+var _bg__imac___1 = document.createElement("div");
+_bg__imac___1.id = "_bg__imac___1";
+_bg__imac___1.style.left = "0px";
+_bg__imac___1.style.top = "0px";
+_bg__imac___1.style.width = "1280px";
+_bg__imac___1.style.height = "720px";
+_bg__imac___1.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(_bg__imac___1);
+
+var ellipse_3 = document.createElement("div");
+ellipse_3.id = "ellipse_3";
+ellipse_3.style.left = "467px";
+ellipse_3.style.top = "-666px";
+ellipse_3.style.width = "1178px";
+ellipse_3.style.height = "1178px";
+ellipse_3.style.borderRadius = "588px / 588px";
+ellipse_3.style.border = "1px solid #54784e";
+ellipse_3.style.background = 'rgba(199.72,225.1,54.17,1)';
+
+content_container.appendChild(ellipse_3);
+
+var _32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1 = document.createElement("div");
+_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1.id = "_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1";
+_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1.style.left = "103px";
+_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1.style.top = "36px";
+_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1.style.position = "absolute";
+_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1.style.width = "45px";
+_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1.style.height = "44px";
+_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1.style.background = "url(skins/_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1.png)";
+
+content_container.appendChild(_32_best_dumpling_recipes__chinese__chicken__japanese_gyoza__potstickers___more__1____copy_1);
+
+var mask_group = document.createElement("div");
+mask_group.id = "mask_group";
+mask_group.style.left = "467px";
+mask_group.style.top = "-663px";
+mask_group.style.position = "absolute";
+mask_group.style.width = "1176px";
+mask_group.style.height = "1176px";
+mask_group.style.background = "url(skins/mask_group.png)";
+
+content_container.appendChild(mask_group);
+
+var the_nutritionist = document.createElement("div");
+the_nutritionist.innerHTML = "THE NUTRITIONIST";
+the_nutritionist.style.textAlign = "left";
+the_nutritionist.id = "the_nutritionist";
+the_nutritionist.style.left = "178px";
+the_nutritionist.style.top = "42px";
+the_nutritionist.style.width = "171px";
+the_nutritionist.style.height = "40px";
+the_nutritionist.style.fontFamily = "Poppins";
+the_nutritionist.style.fontSize = "18px";
+the_nutritionist.style.overflow = "hidden";
+the_nutritionist.style.color = "#000000";
+
+content_container.appendChild(the_nutritionist);
+
+var frame_1 = document.createElement("div");
+frame_1.id = "frame_1";
+frame_1.style.width = "278px";
+frame_1.style.height = "21px";
+frame_1.style.left = "538px";
+frame_1.style.top = "47px";
+frame_1.style.position = "absolute";
+content_container.appendChild(frame_1);
+
+var breakfast = document.createElement("div");
+breakfast.innerHTML = "BREAKFAST";
+breakfast.style.textAlign = "left";
+breakfast.id = "breakfast";
+breakfast.style.left = "0px";
+breakfast.style.top = "0px";
+breakfast.style.width = "87px";
+breakfast.style.height = "31px";
+breakfast.style.fontFamily = "Poppins";
+breakfast.style.fontSize = "14px";
+breakfast.style.overflow = "hidden";
+breakfast.style.color = "#000000";
+
+frame_1.appendChild(breakfast);
+
+var lunch = document.createElement("div");
+lunch.innerHTML = "LUNCH";
+lunch.style.textAlign = "left";
+lunch.id = "lunch";
+lunch.style.left = "129px";
+lunch.style.top = "0px";
+lunch.style.width = "58px";
+lunch.style.height = "31px";
+lunch.style.fontFamily = "Poppins";
+lunch.style.fontSize = "14px";
+lunch.style.overflow = "hidden";
+lunch.style.color = "#000000";
+
+frame_1.appendChild(lunch);
+
+var dinner = document.createElement("div");
+dinner.innerHTML = "DINNER";
+dinner.style.textAlign = "left";
+dinner.id = "dinner";
+dinner.style.left = "229px";
+dinner.style.top = "0px";
+dinner.style.width = "61px";
+dinner.style.height = "31px";
+dinner.style.fontFamily = "Poppins";
+dinner.style.fontSize = "14px";
+dinner.style.overflow = "hidden";
+dinner.style.color = "#000000";
+
+frame_1.appendChild(dinner);
+
+var lock = document.createElement("div");
+lock.id = "lock";
+lock.style.width = "24px";
+lock.style.height = "24px";
+lock.style.left = "1153px";
+lock.style.top = "36px";
+lock.style.position = "absolute";
+content_container.appendChild(lock);
+
+var vector = document.createElement("div");
+vector.id = "vector";
+vector.style.left = "3px";
+vector.style.top = "11px";
+vector.style.position = "absolute";
+vector.style.width = "18px";
+vector.style.height = "11px";
+vector.style.background = "url(skins/vector.png)";
+
+lock.appendChild(vector);
+
+var vector_ek1 = document.createElement("div");
+vector_ek1.id = "vector_ek1";
+vector_ek1.style.left = "7px";
+vector_ek1.style.top = "2px";
+vector_ek1.style.position = "absolute";
+vector_ek1.style.width = "10px";
+vector_ek1.style.height = "9px";
+vector_ek1.style.background = "url(skins/vector_ek1.png)";
+
+lock.appendChild(vector_ek1);
+
+var garlic_butter_chicken_bites_with_lemon_asparagus = document.createElement("div");
+garlic_butter_chicken_bites_with_lemon_asparagus.id = "garlic_butter_chicken_bites_with_lemon_asparagus";
+garlic_butter_chicken_bites_with_lemon_asparagus.style.left = "873px";
+garlic_butter_chicken_bites_with_lemon_asparagus.style.top = "360px";
+garlic_butter_chicken_bites_with_lemon_asparagus.style.position = "absolute";
+garlic_butter_chicken_bites_with_lemon_asparagus.style.width = "137px";
+garlic_butter_chicken_bites_with_lemon_asparagus.style.height = "213px";
+garlic_butter_chicken_bites_with_lemon_asparagus.style.background = "url(skins/garlic_butter_chicken_bites_with_lemon_asparagus.png)";
+
+content_container.appendChild(garlic_butter_chicken_bites_with_lemon_asparagus);
+
+var explantion = document.createElement("div");
+explantion.id = "explantion";
+explantion.style.width = "435px";
+explantion.style.height = "298px";
+explantion.style.left = "103px";
+explantion.style.top = "241px";
+explantion.style.position = "absolute";
+content_container.appendChild(explantion);
+
+var yummy = document.createElement("div");
+yummy.innerHTML = "YUMMY";
+yummy.style.textAlign = "left";
+yummy.id = "yummy";
+yummy.style.left = "0px";
+yummy.style.top = "0px";
+yummy.style.width = "205px";
+yummy.style.height = "106px";
+yummy.style.fontFamily = "Poppins";
+yummy.style.fontSize = "48px";
+yummy.style.overflow = "hidden";
+yummy.style.color = "#000000";
+
+explantion.appendChild(yummy);
+
+var eat_healthy_your_diet_is_a_bank_account = document.createElement("div");
+eat_healthy_your_diet_is_a_bank_account.innerHTML = "EAT HEALTHY,YOUR DIET IS A BANK ACCOUNT";
+eat_healthy_your_diet_is_a_bank_account.style.textAlign = "left";
+eat_healthy_your_diet_is_a_bank_account.id = "eat_healthy_your_diet_is_a_bank_account";
+eat_healthy_your_diet_is_a_bank_account.style.left = "0px";
+eat_healthy_your_diet_is_a_bank_account.style.top = "81px";
+eat_healthy_your_diet_is_a_bank_account.style.width = "458px";
+eat_healthy_your_diet_is_a_bank_account.style.height = "129px";
+eat_healthy_your_diet_is_a_bank_account.style.fontFamily = "Poppins";
+eat_healthy_your_diet_is_a_bank_account.style.fontSize = "36px";
+eat_healthy_your_diet_is_a_bank_account.style.overflow = "hidden";
+eat_healthy_your_diet_is_a_bank_account.style.color = "#000000";
+
+explantion.appendChild(eat_healthy_your_diet_is_a_bank_account);
+
+var welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_ = document.createElement("div");
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.innerHTML = "Welcome to our website  where we make sure that you eat nothing but the best providing you the best options from all around the world with a top notch diet plan. Take a look around and if you like what you see lsign up and subscribe to our monthly plan.";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.style.textAlign = "left";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.id = "welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.style.left = "0px";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.style.top = "194px";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.style.width = "418px";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.style.height = "114px";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.style.fontFamily = "Poppins";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.style.fontSize = "14px";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.style.overflow = "hidden";
+welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_.style.color = "#000000";
+
+explantion.appendChild(welcome_to_our_website_where_we_make_sure_that_you_eat_nothing_but_the_best_providing_you_the_best_options_from_all_around_the_world_with_a_top_notch_diet_plan__take_a_look_around_and_if_you_like_what_you_see_lsign_up_and_subscribe_to_our_monthly_plan_);
+
+var button = document.createElement("div");
+button.id = "button";
+button.style.width = "154px";
+button.style.height = "45px";
+button.style.left = "121px";
+button.style.top = "564px";
+button.style.position = "absolute";
+content_container.appendChild(button);
+
+var button_ek1 = document.createElement("div");
+button_ek1.innerHTML = "START NOW?";
+button_ek1.style.fontWeight = "bold";
+button_ek1.style.textAlign = "left";
+button_ek1.id = "button_ek1";
+button_ek1.style.left = "18px";
+button_ek1.style.top = "9px";
+button_ek1.style.width = "132px";
+button_ek1.style.height = "40px";
+button_ek1.style.fontFamily = "Poppins";
+button_ek1.style.fontSize = "18px";
+button_ek1.style.overflow = "hidden";
+button_ek1.style.color = "#FFFFFF";
+
+button.appendChild(button_ek1);
+
+var garlic_butter_steak_bites_with_lemon_zucchini_noodles = document.createElement("div");
+garlic_butter_steak_bites_with_lemon_zucchini_noodles.id = "garlic_butter_steak_bites_with_lemon_zucchini_noodles";
+garlic_butter_steak_bites_with_lemon_zucchini_noodles.style.left = "941px";
+garlic_butter_steak_bites_with_lemon_zucchini_noodles.style.top = "141px";
+garlic_butter_steak_bites_with_lemon_zucchini_noodles.style.position = "absolute";
+garlic_butter_steak_bites_with_lemon_zucchini_noodles.style.width = "84px";
+garlic_butter_steak_bites_with_lemon_zucchini_noodles.style.height = "100px";
+garlic_butter_steak_bites_with_lemon_zucchini_noodles.style.background = "url(skins/garlic_butter_steak_bites_with_lemon_zucchini_noodles.png)";
+
+content_container.appendChild(garlic_butter_steak_bites_with_lemon_zucchini_noodles);
+
+var garlic_mushrooms_cauliflower_skillet = document.createElement("div");
+garlic_mushrooms_cauliflower_skillet.id = "garlic_mushrooms_cauliflower_skillet";
+garlic_mushrooms_cauliflower_skillet.style.left = "812px";
+garlic_mushrooms_cauliflower_skillet.style.top = "160px";
+garlic_mushrooms_cauliflower_skillet.style.position = "absolute";
+garlic_mushrooms_cauliflower_skillet.style.width = "61.43px";
+garlic_mushrooms_cauliflower_skillet.style.height = "100px";
+garlic_mushrooms_cauliflower_skillet.style.background = "url(skins/garlic_mushrooms_cauliflower_skillet.png)";
+
+content_container.appendChild(garlic_mushrooms_cauliflower_skillet);
+
+var easy_saucy_ramen_noodles__vegan_recipe_ = document.createElement("div");
+easy_saucy_ramen_noodles__vegan_recipe_.id = "easy_saucy_ramen_noodles__vegan_recipe_";
+easy_saucy_ramen_noodles__vegan_recipe_.style.left = "1082px";
+easy_saucy_ramen_noodles__vegan_recipe_.style.top = "210px";
+easy_saucy_ramen_noodles__vegan_recipe_.style.position = "absolute";
+easy_saucy_ramen_noodles__vegan_recipe_.style.width = "71.1px";
+easy_saucy_ramen_noodles__vegan_recipe_.style.height = "100px";
+easy_saucy_ramen_noodles__vegan_recipe_.style.background = "url(skins/easy_saucy_ramen_noodles__vegan_recipe_.png)";
+
+content_container.appendChild(easy_saucy_ramen_noodles__vegan_recipe_);
+
+var _25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy = document.createElement("div");
+_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy.id = "_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy";
+_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy.style.left = "694px";
+_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy.style.top = "260px";
+_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy.style.position = "absolute";
+_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy.style.width = "47.86px";
+_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy.style.height = "100px";
+_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy.style.background = "url(skins/_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy.png)";
+
+content_container.appendChild(_25_easy_meal_prep_ideas_for_when_you_have_no_idea_what_to_cook___copy);
+
+var asado_chicken_and_sauteed_lemon_zucchini = document.createElement("div");
+asado_chicken_and_sauteed_lemon_zucchini.id = "asado_chicken_and_sauteed_lemon_zucchini";
+asado_chicken_and_sauteed_lemon_zucchini.style.left = "1139px";
+asado_chicken_and_sauteed_lemon_zucchini.style.top = "366px";
+asado_chicken_and_sauteed_lemon_zucchini.style.position = "absolute";
+asado_chicken_and_sauteed_lemon_zucchini.style.width = "75.97px";
+asado_chicken_and_sauteed_lemon_zucchini.style.height = "100px";
+asado_chicken_and_sauteed_lemon_zucchini.style.background = "url(skins/asado_chicken_and_sauteed_lemon_zucchini.png)";
+
+content_container.appendChild(asado_chicken_and_sauteed_lemon_zucchini);
+
